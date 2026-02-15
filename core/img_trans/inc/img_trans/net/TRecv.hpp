fdfde1a54b0077ae24d59ca177c775b0c257a6d7
Loris Rosa <162586864+VelVenn@users.noreply.github.com>
2026-02-16 01:36:51 +0800

    Added new img trans networking headers

---
+++ core/img_trans/inc/img_trans/net/TRecv.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "img_trans/net/TReassembly.hpp"
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/errno.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+class TRecv {
+
+};
